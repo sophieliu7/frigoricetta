bd9044857fb2843bcd3d505293265fbec6a56857
Clara Besnard <clarabesnard@gmail.com>
2018-08-28 12:44:11 +0200

    controller et validations

--- app/controllers/user_products_controller.rb
+++ app/controllers/user_products_controller.rb
@@ -1,22 +1,43 @@
 class UserProductsController < ApplicationController
-  def index
-  end
+  before_action :set_user_product, only: [:edit, :update, :destroy]
 
-  def show
+  def index
+    @user_products = UserProduct.where(user_id: current_user)
   end
 
   def new
+    @user_product = UserProduct.new
   end
 
   def create
+    UserProduct.create!(user_products_params)
+    redirect_to user_products_path
   end
 
   def edit
   end
 
   def update
+    @user_product.update(user_products_params)
+    redirect_to user_products_path
   end
 
   def destroy
+    @user_product.destroy
+    redirect_to user_products_path
+  end
+
+  private
+
+  def user_products_params
+    params.require(:user_product).permit(
+      :place,
+      :purchase_date,
+      :peremption_date
+      )
+  end
+
+  def set_user_product
+    @user_product = UserProduct.find(params[:id])
   end
 end
--- app/models/user_product.rb
+++ app/models/user_product.rb
@@ -1,4 +1,8 @@
 class UserProduct < ApplicationRecord
   belongs_to :user
   belongs_to :product
+
+  validates :user_id, presence: true
+  validates :product_id, presence: true
+  validates :purchase_date, presence: true
 end
