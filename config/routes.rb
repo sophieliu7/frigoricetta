61dc67aa3b5715a542a22b91866d9266f8b71c4c
benoit gallou <benoit.gallou@gmail.com>
2018-08-29 11:12:41 +0200

    ajout nokogiri dans gem file et creation route recette et ajout action recette dans controller pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
   resources :user_products
+
+
   devise_for :users
   root to: 'pages#home'
+
+  get 'pages/recettes', to: 'pages#recettes', as: :recette
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
+
