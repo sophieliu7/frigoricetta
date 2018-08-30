d2e85ab785051ad8daab5fc295052dd6fb8d9275
sophie liu <sophieliu@hotmail.fr>
2018-08-30 19:31:05 +0200

    changer NAVBAR

--- app/javascript/components/navbar.js
+++ app/javascript/components/navbar.js
@@ -1,7 +1,8 @@
 function initUpdateNavbarOnScroll() {
   const navbar = document.querySelector('.navbar-wagon');
+  const banner = document.querySelector('.banner');
 
-  if (navbar) {
+  if (navbar && banner) {
     window.addEventListener('scroll', () => {
       if (window.scrollY >= window.innerHeight) {
         navbar.classList.add('navbar-wagon-white');
